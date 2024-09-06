da3f9c8e6ba7c76c7a337ff764a3f147f9da055d
Davit Torchyan <daviddt.torchyan@gmail.com>
2024-09-06 18:57:43 +0400

    personalize manifests with correct repo name

--- personalize.sh
+++ personalize.sh
@@ -6,16 +6,23 @@ case "$(uname)" in
   *) sed_i_opt=(-E -i) ;;           # Linux
 esac
 
+# Prompt for GitHub username/org and repo name
 echo -n "Enter GitHub username/org: "
 read username
 
-find . -type f -name '*.yaml' -exec sed "${sed_i_opt[@]}" s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
-find . -type f -name '*.yaml' -exec sed "${sed_i_opt[@]}" s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
+echo -n "Enter the repository name (e.g. bluebird-kargo): "
+read repo_name
 
+# Replace GitHub username/org and repository name in .yaml files
+find . -type f -name '*.yaml' -exec sed "${sed_i_opt[@]}" "s#https://github.com/[-_a-zA-Z0-9]+/[-_a-zA-Z0-9]+#https://github.com/${username}/${repo_name}#g" {} +
+find . -type f -name '*.yaml' -exec sed "${sed_i_opt[@]}" "s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g" {} +
+
+# Prompt for Argo CD destination name or server
 echo "Enter Argo CD destination name or server where applications will be deployed (e.g. in-cluster, https://kubernetes.default.svc)"
 echo -n "Destination: "
 read destination
 
+# Update Argo CD destination settings
 if [[ $destination == https:* ]]; then
   sed -i.bak -E "s@^        (# )?server:.*@        server: ${destination}@g" argocd/appset.yaml
   sed -i.bak -E "s@^        (# )?name:.*@        # name: REPLACEME@g" argocd/appset.yaml
@@ -24,7 +31,6 @@ else
   sed -i.bak -E "s@^        (# )?server:.*@        # server: https://REPLACEME@g" argocd/appset.yaml
 fi
 rm -f argocd/appset.yaml.bak
-
 
 # #!/bin/bash
 
