60900150fcdcdc27f338e9bb735e14d4b91c25ee
Davit Torchyan <daviddt.torchyan@gmail.com>
2024-09-06 18:51:41 +0400

    personalize manifests

--- personalize.sh
+++ personalize.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Detect OS type to decide on the sed -i syntax
+case "$(uname)" in
+  Darwin*) sed_i_opt=(-E -i '') ;;  # macOS
+  *) sed_i_opt=(-E -i) ;;           # Linux
+esac
+
 echo -n "Enter GitHub username/org: "
 read username
 
-find . -type f -name '*.yaml' -exec sed -E -i '' s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
-find . -type f -name '*.yaml' -exec sed -E -i '' s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
+find . -type f -name '*.yaml' -exec sed "${sed_i_opt[@]}" s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
+find . -type f -name '*.yaml' -exec sed "${sed_i_opt[@]}" s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
 
 echo "Enter Argo CD destination name or server where applications will be deployed (e.g. in-cluster, https://kubernetes.default.svc)"
 echo -n "Destination: "
@@ -18,3 +24,25 @@ else
   sed -i.bak -E "s@^        (# )?server:.*@        # server: https://REPLACEME@g" argocd/appset.yaml
 fi
 rm -f argocd/appset.yaml.bak
+
+
+# #!/bin/bash
+
+# echo -n "Enter GitHub username/org: "
+# read username
+
+# find . -type f -name '*.yaml' -exec sed -E -i '' s#https://github.com/[-_a-zA-Z0-9]+#https://github.com/${username}#g {} +
+# find . -type f -name '*.yaml' -exec sed -E -i '' s#ghcr.io/[-_a-zA-Z0-9]+#ghcr.io/${username}#g {} +
+
+# echo "Enter Argo CD destination name or server where applications will be deployed (e.g. in-cluster, https://kubernetes.default.svc)"
+# echo -n "Destination: "
+# read destination
+
+# if [[ $destination == https:* ]]; then
+#   sed -i.bak -E "s@^        (# )?server:.*@        server: ${destination}@g" argocd/appset.yaml
+#   sed -i.bak -E "s@^        (# )?name:.*@        # name: REPLACEME@g" argocd/appset.yaml
+# else
+#   sed -i.bak -E "s@^        (# )?name:.*@        name: ${destination}@g" argocd/appset.yaml
+#   sed -i.bak -E "s@^        (# )?server:.*@        # server: https://REPLACEME@g" argocd/appset.yaml
+# fi
+# rm -f argocd/appset.yaml.bak
